df9da9c0b2b4c22559add9bbd5520fe196640033
Ramiro Gómez <code@ramiro.org>
2015-02-06 22:38:26 +0100

    added bundle download

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,8 @@ cd ../d3-geomap.github.io/
 shopt -s extglob
 rm -r !(.git|.|..)
 cp -r ../site/deploy/* .
+mkdir download
+cp ../d3-geomap/bundle/*.zip download/
 git add . -A
 git commit -am'deploy'
 git push
