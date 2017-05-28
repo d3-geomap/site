c60dc13a28861f3422b8e568162a635252c5b84e
Ramiro Gómez <code@ramiro.org>
2017-05-28 22:59:38 +0200

    More comprehensible deploy steps

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 set -euo pipefail
+ping=${1:-}
+
+deploy_repo=~/repos/deploy/d3-geomap.github.io
 
 logya gen
-cd ../d3-geomap.github.io/
-shopt -s extglob
-rm -r !(.git|.|..)
-cp -r ../site/deploy/* .
-mkdir download
-cp ../d3-geomap/bundle/*.zip download/
+cp -r deploy/* "$deploy_repo"
+
+cd "$deploy_repo"
+mkdir -p download
+cp ~/repos/pub/d3-geomap/d3-geomap/bundle/*.zip download/
 git add . -Af # use the force so globally ignored zip gets added
 git commit -am'deploy'
 git push
